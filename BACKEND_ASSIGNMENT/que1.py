7a11b8bd3fbfdd3b97c15c31baf442bd32706b13
Zeba Khan <160715206+zebakhan694@users.noreply.github.com>
2025-01-27 09:41:38 +0530

    Delete BACKEND_ASSIGNMENT directory

--- BACKEND_ASSIGNMENT/que1.py
+++
@@ -1,4 +0,0 @@
-# Write a Python program that prints "Hello World"
-   
-print("Hello World")
-
--- BACKEND_ASSIGNMENT/que2.py
+++
@@ -1,3 +0,0 @@
-# set up Python on your local machine and write a program to display your name.
-
-print("Zeba Khan")
